e019fb10d4b8bd8bdc221c65204adde866c692a0
iSellgren <frse1700@student.miun.se>
2018-02-26 12:26:39 +0100

    Ny startsida

--- app/src/main/java/com/example/isellgren/suncatcher/BeginActivity.java
+++ app/src/main/java/com/example/isellgren/suncatcher/BeginActivity.java
@@ -1,28 +1,28 @@
 package com.example.isellgren.suncatcher;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-
-import com.google.android.gms.maps.model.Circle;
+import android.view.Window;
 
 import static com.example.isellgren.suncatcher.R.layout.activity_begin;
 
-public class BeginActivity extends AppCompatActivity {
+public class BeginActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(activity_begin);
 
     }
 
     public void onInfoClick(MenuItem item) {
         {
-            if(item.getItemId() == R.id.mejl)
-            {
+            if (item.getItemId() == R.id.mejl) {
                 Intent j = new Intent(BeginActivity.this, Mail.class);
                 startActivity(j);
 
@@ -32,17 +32,16 @@ public class BeginActivity extends AppCompatActivity {
 
     public void onBlueClick(MenuItem item) {
         {
-            if(item.getItemId() == R.id.bluetand)
-            {
+            if (item.getItemId() == R.id.bluetand) {
                 Intent k = new Intent(BeginActivity.this, Bluetand.class);
                 startActivity(k);
             }
         }
     }
+
     public void onWifiClick(MenuItem item) {
 
-        if(item.getItemId() == R.id.wifi)
-        {
+        if (item.getItemId() == R.id.wifi) {
             Intent l = new Intent(BeginActivity.this, Wifi.class);
             startActivity(l);
         }
@@ -50,29 +49,32 @@ public class BeginActivity extends AppCompatActivity {
 
     public void onLogregClick(MenuItem item) {
 
-        if(item.getItemId() == R.id.logreg)
-        {
+        if (item.getItemId() == R.id.logreg) {
             Intent m = new Intent(BeginActivity.this, LoginActivity.class);
             startActivity(m);
         }
     }
 
-    public void onstatsClick(View view) {
+    public void onstatsClick(MenuItem item) {
         Intent n = new Intent(BeginActivity.this, Statistic.class);
         startActivity(n);
     }
 
-    public void onprofileClick(View view) {
+    public void onprofileClick(MenuItem item) {
         Intent o = new Intent(BeginActivity.this, Profile.class);
         startActivity(o);
     }
 
-    public void ondeviceClick(View view) {
+    public void ondeviceClick(MenuItem item) {
         Intent p = new Intent(BeginActivity.this, DeviceActivity.class);
         startActivity(p);
     }
 
-    public void onpsettingClick(View view) {
+
+    public void onpsettingClick(MenuItem item) {
+        Intent p = new Intent(BeginActivity.this, DeviceActivity.class);
+        startActivity(p);
     }
+
 }
 
