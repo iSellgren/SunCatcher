3e4d2e69a96308d026e47379801471c795ffea43
iSellgren <iSellgren@DESKTOP-NHVAFUS>
2018-04-18 15:46:46 +0200

    ....

--- app/src/main/java/com/example/isellgren/suncatcher/LoginActivity.java
+++ app/src/main/java/com/example/isellgren/suncatcher/LoginActivity.java
@@ -1,65 +1,167 @@
 package com.example.isellgren.suncatcher;
-
+import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
+import android.os.StrictMode;
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+    /*
+        This source code could be used for academic purposes only. Posting on other websites or blogs is only allowed with a dofollow link to the orignal content.
+    */
+
+public class LoginActivity extends AppCompatActivity
+{
+    // Declaring layout button, edit texts
+    Button login;
+    EditText username,password;
+    ProgressBar progressBar;
+    // End Declaring layout button, edit texts
+
+    // Declaring connection variables
+    Connection con;
+    String un,pass,db,ip;
+    //End Declaring connection variables
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
 
+        // Getting values from button, texts and progress bar
+        login = (Button) findViewById(R.id.button);
+        username = (EditText) findViewById(R.id.editText);
+        password = (EditText) findViewById(R.id.editText2);
+        // End Getting values from button, texts and progress bar
 
-        final EditText etUsername =(EditText) findViewById(R.id.etDown);
-        final EditText etPasssword =(EditText) findViewById(R.id.etPassword);
-        final Button bLogin =(Button) findViewById(R.id.bLogin);
-        final TextView registerLink =(TextView) findViewById(R.id.tvRegisterHere);
+        // Declaring Server ip, username, database name and password
+        ip = "192.168.43.145";
+        db = "bits";
+        un = "K";
+        pass = "K";
+        // Declaring Server ip, username, database name and password
 
-        registerLink.setOnClickListener(new View.OnClickListener() {
+        // Setting up the function when button login is clicked
+        login.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
-                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
-                LoginActivity.this.startActivity(registerIntent);
+            public void onClick(View v)
+            {
+                CheckLogin checkLogin = new CheckLogin();// this is the Asynctask, which is used to process in background to reduce load on app process
+                checkLogin.execute("");
             }
         });
-
+        //End Setting up the function when button login is clicked
     }
 
-    public void onInfoClick(MenuItem item) {
-        {
-            if(item.getItemId() == R.id.mejl)
-            {
-                Intent j = new Intent(LoginActivity.this, Mail.class);
-                startActivity(j);
+    public class CheckLogin extends AsyncTask<String,String,String>
+    {
+        String z = "";
+        Boolean isSuccess = false;
 
+        @Override
+        protected void onPreExecute()
+        {
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected void onPostExecute(String r)
+        {
+            progressBar.setVisibility(View.GONE);
+            Toast.makeText(LoginActivity.this, r, Toast.LENGTH_SHORT).show();
+            if(isSuccess)
+            {
+                Toast.makeText(LoginActivity.this , "Login Successfull" , Toast.LENGTH_LONG).show();
+                //finish();
             }
         }
-    }
-
-    public void onBlueClick(MenuItem item) {
+        @Override
+        protected String doInBackground(String... params)
         {
-            if(item.getItemId() ==R.id.bluetand)
+            String usernam = username.getText().toString();
+            String passwordd = password.getText().toString();
+            if(usernam.trim().equals("")|| passwordd.trim().equals(""))
+                z = "Please enter Username and Password";
+            else
             {
-                Intent k = new Intent(LoginActivity.this, Bluetand.class);
-                startActivity(k);
+                try
+                {
+                    con = connectionclass(un, pass, db, ip);        // Connect to database
+                    if (con == null)
+                    {
+                        z = "Check Your Internet Access!";
+                    }
+                    else
+                    {
+                        // Change below query according to your own database.
+                        String query = "select * from login where user_name= '" + usernam.toString() + "' and pass_word = '"+ passwordd.toString() +"'  ";
+                        Statement stmt = con.createStatement();
+                        ResultSet rs = stmt.executeQuery(query);
+                        if(rs.next())
+                        {
+                            z = "Login successful";
+                            isSuccess=true;
+                            con.close();
+                        }
+                        else
+                        {
+                            z = "Invalid Credentials!";
+                            isSuccess = false;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    isSuccess = false;
+                    z = ex.getMessage();
+                }
             }
+            return z;
         }
     }
-    public void onWifiClick(MenuItem item) {
 
-        if(item.getItemId() == R.id.wifi)
+
+    @SuppressLint("NewApi")
+    public Connection connectionclass(String user, String password, String database, String server)
+    {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection connection = null;
+        String ConnectionURL = null;
+        try
         {
-            Intent l = new Intent(LoginActivity.this, Wifi.class);
-            startActivity(l);
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            ConnectionURL = "jdbc:jtds:sqlserver://" + server + database + ";user=" + user+ ";password=" + password + ";";
+            connection = DriverManager.getConnection(ConnectionURL);
         }
+        catch (SQLException se)
+        {
+            Log.e("error here 1 : ", se.getMessage());
+        }
+        catch (ClassNotFoundException e)
+        {
+            Log.e("error here 2 : ", e.getMessage());
+        }
+        catch (Exception e)
+        {
+            Log.e("error here 3 : ", e.getMessage());
+        }
+        return connection;
     }
-
-
 }
+                
