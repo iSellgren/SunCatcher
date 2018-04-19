1757ee2d92a8645f947ccdff19d6a4f64ff24803
iSellgren <iSellgren@DESKTOP-NHVAFUS>
2018-04-19 18:53:48 +0200

    Örnen har landat

--- app/src/main/java/com/example/isellgren/suncatcher/LoginActivity.java
+++ app/src/main/java/com/example/isellgren/suncatcher/LoginActivity.java
@@ -4,8 +4,13 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -17,7 +22,8 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class LoginActivity extends Activity {
-
+    private static SeekBar seek_bar;
+    private static TextView text_view;
     EditText up, down;
     String Up, Down;
     Context ctx=this;
@@ -26,18 +32,50 @@ public class LoginActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        up = (EditText) findViewById(R.id.Up);
-        down = (EditText) findViewById(R.id.Down);
+        seebbar();
 
     }
 
     public void Send_btn(View v){
-        Up = up.getText().toString();
-        Down = down.getText().toString();
 
-        BackGround b = new BackGround();
-        b.execute(Up, Down);
+
     }
+
+    public void seebbar( ){
+        seek_bar = (SeekBar)findViewById(R.id.seekBar);
+        text_view =(TextView)findViewById(R.id.textView);
+        text_view.setText("Covered : " + seek_bar.getProgress() + " / " +seek_bar.getMax());
+
+
+        seek_bar.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+
+                    int progress_value;
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        progress_value = progress;
+                        text_view.setText("Covered : " + progress + " / " +seek_bar.getMax());
+                        Toast.makeText(LoginActivity.this,"SeekBar in progress",Toast.LENGTH_LONG).show();
+                        BackGround b = new BackGround();
+
+                        b.execute(String.valueOf(progress_value), "0");
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+                        //Toast.makeText(LoginActivity.this,"SeekBar in StartTracking",Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+                        //text_view.setText("Covered : " + progress_value + " / " +seek_bar.getMax());
+                        //Toast.makeText(LoginActivity.this,"SeekBar in StopTracking",Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+
+    }
+
 
 
     class BackGround extends AsyncTask<String, String, String> {
@@ -84,7 +122,7 @@ public class LoginActivity extends Activity {
             if(s.equals("")){
                 s="Data saved successfully.";
             }
-            Toast.makeText(ctx, s, Toast.LENGTH_LONG).show();
+            //Toast.makeText(ctx, s, Toast.LENGTH_LONG).show();
         }
     }
 
